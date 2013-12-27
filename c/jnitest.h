0c361201e2f32431849eeac47eb23a3e98d42f67
gabor-farkas <gabor.farkas@doctusoft.com>
2013-12-27 19:14:49 +0100

    removed old files

--- c/jnitest.h
+++
@@ -1,6 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jlong JNICALL Java_pipwm4j_Pwm_allocateControlStructure(JNIEnv *, jobject, jint pin);
-JNIEXPORT void JNICALL Java_pipwm4j_Pwm_writeControlRegister(JNIEnv *, jobject, jlong pwmSpan, jlong fill, jboolean stop);
-JNIEXPORT void JNICALL Java_pipwm4j_Pwm_doPwm(JNIEnv *, jobject, jlong controlRegister);
-JNIEXPORT void JNICALL Java_pipwm4j_Pwm_freeControlStructure(JNIEnv *, jobject, jlong controlRegister);
