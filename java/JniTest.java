daf7d1b6693f095379b5efbdca9294f08a1ebce8
gabor-farkas <gabor.farkas@doctusoft.com>
2013-12-27 18:52:23 +0100

    renamed jnitest to pwm

--- java/JniTest.java
+++ java/JniTest.java
@@ -4,7 +4,7 @@ import pipwm4j.Pwm;
 public class JniTest {
 
     public static void main(String [] args) {
-	System.loadLibrary("jnitest");
+	System.loadLibrary("pwm");
 	Pwm pwm = new Pwm(1);
 	pwm.close();
     }
